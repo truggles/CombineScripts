1fb4b5b2ab5755855e1a1014285bcd53449b90b8
Tyler Ruggles <truggles@wisc.edu>
2018-03-25 22:07:09 +0200

    For making post-fit plots in 81X from fitDiagnosts inputs

---
+++ VH2016/plot_VH_postfit81X.py
@@ -0,0 +1,511 @@
+#!/usr/bin/env python
+import ROOT
+import re
+from array import array
+import argparse
+from poisson import convert
+from poisson import poisson_errors
+from poisson import getTH1FfromTGraphAsymmErrors
+
+parser = argparse.ArgumentParser(
+    "Create pre/post-fit plots for SM HTT")
+parser.add_argument(
+    "--fs",
+    action="store",
+    dest="fs",
+    default="emt",
+    help="Which channel to run over? (emt, mmt, ett, mtt, whlep, whhad, wh, zh, eeem, eeet, eemt, eett, emmm, emmt, mmtt, mmmt, llem, llet, llmt, lltt)")
+args = parser.parse_args()
+fs=args.fs
+
+def add_lumi():
+    lowX=0.58
+    lowY=0.835
+    lumi  = ROOT.TPaveText(lowX, lowY+0.06, lowX+0.30, lowY+0.16, "NDC")
+    lumi.SetBorderSize(   0 )
+    lumi.SetFillStyle(    0 )
+    lumi.SetTextAlign(   12 )
+    lumi.SetTextColor(    1 )
+    lumi.SetTextSize(0.06)
+    lumi.SetTextFont (   42 )
+    lumi.AddText("35.9 fb^{-1} (13 TeV)")
+    return lumi
+
+def add_CMS():
+    lowX=0.21
+    lowY=0.70
+    lumi  = ROOT.TPaveText(lowX, lowY+0.06, lowX+0.15, lowY+0.16, "NDC")
+    lumi.SetTextFont(61)
+    lumi.SetTextSize(0.08)
+    lumi.SetBorderSize(   0 )
+    lumi.SetFillStyle(    0 )
+    lumi.SetTextAlign(   12 )
+    lumi.SetTextColor(    1 )
+    lumi.AddText("CMS")
+    return lumi
+
+def add_Preliminary():
+    lowX=0.21
+    lowY=0.63
+    lumi  = ROOT.TPaveText(lowX, lowY+0.06, lowX+0.15, lowY+0.16, "NDC")
+    lumi.SetTextFont(52)
+    lumi.SetTextSize(0.06)
+    lumi.SetBorderSize(   0 )
+    lumi.SetFillStyle(    0 )
+    lumi.SetTextAlign(   12 )
+    lumi.SetTextColor(    1 )
+    lumi.AddText("Preliminary")
+    return lumi
+
+def make_legend():
+        #output = ROOT.TLegend(0.70, 0.40, 0.92, 0.84, "", "brNDC")
+        output = ROOT.TLegend(0.45, 0.60, 0.95, 0.84, "", "brNDC")
+        output.SetLineWidth(0)
+        output.SetLineStyle(0)
+        output.SetFillStyle(0)
+        output.SetBorderSize(0)
+ 	output.SetNColumns(2)
+        output.SetTextFont(62)
+        return output
+
+ROOT.gROOT.SetBatch(True)
+ROOT.gStyle.SetFrameLineWidth(3)
+ROOT.gStyle.SetLineWidth(3)
+ROOT.gStyle.SetOptStat(0)
+
+c=ROOT.TCanvas("canvas","",0,0,600,600)
+c.cd()
+
+#file=ROOT.TFile("vh_Post.root","r")
+#file=ROOT.TFile("postfit_wh.root","r")
+#file=ROOT.TFile("fitDiagnostics.root","r")
+file=ROOT.TFile("fitDiagnostics_asimov.root","r")
+print file
+
+adapt=ROOT.gROOT.GetColor(12)
+new_idx=ROOT.gROOT.GetListOfColors().GetSize() + 1
+trans=ROOT.TColor(new_idx, adapt.GetRed(), adapt.GetGreen(),adapt.GetBlue(), "",0.5)
+
+channels=[]
+if fs=="emt":
+  channels=["ch9"]
+if fs=="mmt":
+  channels=["ch11"]
+if fs=="ett":
+  channels=["ch10"]
+if fs=="mtt":
+  channels=["ch12"]
+if fs=="eeem":
+  channels=["ch1"]
+if fs=="eeet":
+  channels=["ch2"]
+if fs=="eemt":
+  channels=["ch3"]
+if fs=="eett":
+  channels=["ch4"]
+if fs=="emmm":
+  channels=["ch5"]
+if fs=="emmt":
+  channels=["ch6"]
+if fs=="mmmt":
+  channels=["ch7"]
+if fs=="mmtt":
+  channels=["ch8"]
+if fs=="llem":
+  channels=["ch1","ch5"]
+if fs=="llet":
+  channels=["ch2","ch6"]
+if fs=="llmt":
+  channels=["ch3","ch7"]
+if fs=="lltt":
+  channels=["ch4","ch8"]
+if fs=="zh":
+  channels=["ch1","ch5","ch2","ch6","ch3","ch7","ch4","ch8"]
+if fs=="whlep":
+  channels=["ch9","ch11"]
+if fs=="whhad":
+  channels=["ch10","ch12"]
+if fs=="wh":
+  channels=["ch9","ch11","ch10","ch12"]
+
+nchan=len(channels)
+print fs
+print channels
+
+prepend = "shapes_fit_s/"
+
+print file.Get(prepend+channels[0])
+print file.Get(prepend+channels[0]).Get("data")
+data_graph=file.Get(prepend+channels[0]).Get("data")
+Data=getTH1FfromTGraphAsymmErrors( data_graph, "data" )
+Data.Sumw2(ROOT.kFALSE)
+Data.SetBinErrorOption(ROOT.TH1.kPoisson)
+#Data=file.Get(prepend+channels[0]).Get("data")
+print Data
+ZZ=file.Get(prepend+channels[0]).Get("ZZ")
+if file.Get(prepend+channels[0]).Get("ggZZ"):
+  ZZ.Add(file.Get(prepend+channels[0]).Get("ggZZ"))
+WZ=file.Get(prepend+channels[0]).Get("WZ")
+print file.Get(prepend+channels[0]).Get("WZ"),file.Get(prepend+channels[0]).Get("WZ1")
+Rare=file.Get(prepend+channels[0]).Get("ttZ")
+if file.Get(prepend+channels[0]).Get("TT"):
+ Rare.Add(file.Get(prepend+channels[0]).Get("TT"))
+Fakes=ZZ.Clone()
+if file.Get(prepend+channels[0]).Get("allFakes"):
+  Fake=file.Get(prepend+channels[0]).Get("allFakes")
+if file.Get(prepend+channels[0]).Get("RedBkg"):
+  Fake=file.Get(prepend+channels[0]).Get("RedBkg")
+if file.Get(prepend+channels[0]).Get("jetFakes"):
+  Fake=file.Get(prepend+channels[0]).Get("jetFakes")
+print prepend+channels[0]
+WH=file.Get(prepend+channels[0]).Get("WH_htt")
+ZH=file.Get(prepend+channels[0]).Get("ZH_htt")
+Total=file.Get(prepend+channels[0]).Get("total_background")
+if file.Get(prepend+channels[0]).Get("DY"):
+  Rare.Add(file.Get(prepend+channels[0]).Get("DY"))
+if file.Get(prepend+channels[0]).Get("ttW"): 
+  Rare.Add(file.Get(prepend+channels[0]).Get("ttW"))
+if file.Get(prepend+channels[0]).Get("TriBoson"):
+  Rare.Add(file.Get(prepend+channels[0]).Get("TriBoson"))
+if file.Get(prepend+channels[0]).Get("WH_hww125"):
+  Rare.Add(file.Get(prepend+channels[0]).Get("WH_hww125"))
+if file.Get(prepend+channels[0]).Get("ZH_hww125"):
+  Rare.Add(file.Get(prepend+channels[0]).Get("ZH_hww125"))
+if file.Get(prepend+channels[0]).Get("ggH_hzz125"):
+  Rare.Add(file.Get(prepend+channels[0]).Get("ggH_hzz125"))
+if "ch9" not in prepend+channels[0] and file.Get(prepend+channels[0]).Get("WZ"):
+  Rare.Add(file.Get(prepend+channels[0]).Get("WZ"))
+
+for i in range (1,nchan):
+   data_graph=file.Get(prepend+channels[i]).Get("data")
+   data_hist=getTH1FfromTGraphAsymmErrors( data_graph )
+   data_hist.Sumw2(ROOT.kFALSE)
+   data_hist.SetBinErrorOption(ROOT.TH1.kPoisson)
+   Data.Add(data_hist)
+   ZZ.Add(file.Get(prepend+channels[i]).Get("ZZ"))
+   if file.Get(prepend+channels[i]).Get("ggZZ"):
+     ZZ.Add(file.Get(prepend+channels[i]).Get("ggZZ"))
+   if "ch9" in prepend+channels[i]:
+     WZ.Add(file.Get(prepend+channels[i]).Get("WZ"))
+   if file.Get(prepend+channels[i]).Get("DY"): 
+     Rare.Add(file.Get(prepend+channels[i]).Get("DY"))
+   if file.Get(prepend+channels[i]).Get("TT"): 
+     Rare.Add(file.Get(prepend+channels[i]).Get("TT"))
+   if file.Get(prepend+channels[i]).Get("allFakes"): 
+     Fake.Add(file.Get(prepend+channels[i]).Get("allFakes"))
+   if file.Get(prepend+channels[i]).Get("RedBkg"): 
+     Fake.Add(file.Get(prepend+channels[i]).Get("RedBkg"))
+   if file.Get(prepend+channels[i]).Get("jetFakes"): 
+     Fake.Add(file.Get(prepend+channels[i]).Get("jetFakes"))
+   WH.Add(file.Get(prepend+channels[i]).Get("WH_htt"))
+   ZH.Add(file.Get(prepend+channels[i]).Get("ZH_htt"))
+   if file.Get(prepend+channels[i]).Get("ttZ"):
+     Rare.Add(file.Get(prepend+channels[i]).Get("ttZ"))
+   if file.Get(prepend+channels[i]).Get("ttW"):
+     Rare.Add(file.Get(prepend+channels[i]).Get("ttW"))
+   if file.Get(prepend+channels[i]).Get("TriBoson"):
+     Rare.Add(file.Get(prepend+channels[i]).Get("TriBoson"))
+   if file.Get(prepend+channels[i]).Get("WH_hww125"):
+     Rare.Add(file.Get(prepend+channels[i]).Get("WH_hww125"))
+   if file.Get(prepend+channels[i]).Get("ZH_hww125"):
+     Rare.Add(file.Get(prepend+channels[i]).Get("ZH_hww125"))
+   if file.Get(prepend+channels[i]).Get("ggH_hzz125"): 
+     Rare.Add(file.Get(prepend+channels[i]).Get("ggH_hzz125"))
+   if "ch9" not in prepend+channels[i] and file.Get(prepend+channels[i]).Get("WZ"):
+     Rare.Add(file.Get(prepend+channels[i]).Get("WZ"))
+
+   Total.Add(file.Get(prepend+channels[i]).Get("total_background"))
+
+WH.Scale(5)
+ZH.Scale(5)
+
+WH.GetXaxis().SetTitle("")
+WH.GetXaxis().SetTitleSize(0)
+WH.GetXaxis().SetNdivisions(505)
+WH.GetYaxis().SetLabelFont(42)
+WH.GetYaxis().SetLabelOffset(0.01)
+WH.GetYaxis().SetLabelSize(0.06)
+WH.GetYaxis().SetTitleSize(0.075)
+WH.GetYaxis().SetTitleOffset(1.04)
+WH.SetTitle("")
+WH.GetYaxis().SetTitle("Events/bin")
+WH.SetMinimum(0)
+WH.SetMarkerStyle(20)
+WH.SetMarkerSize(1)
+WH.GetXaxis().SetLabelSize(0)
+
+Data.Sumw2(ROOT.kFALSE)
+Data.SetBinErrorOption(ROOT.TH1.kPoisson)
+Data.GetXaxis().SetTitle("")
+Data.GetXaxis().SetTitleSize(0)
+Data.GetXaxis().SetNdivisions(505)
+Data.GetYaxis().SetLabelFont(42)
+Data.GetYaxis().SetLabelOffset(0.01)
+Data.GetYaxis().SetLabelSize(0.06)
+Data.GetYaxis().SetTitleSize(0.075)
+Data.GetYaxis().SetTitleOffset(1.04)
+Data.SetTitle("")
+Data.GetYaxis().SetTitle("Events/bin")
+
+if "ch9" in channels[0]:
+ WZ.SetFillColor(ROOT.TColor.GetColor("#efe7ae"))
+ZZ.SetFillColor(ROOT.TColor.GetColor("#11e7ae"))
+Fake.SetFillColor(ROOT.TColor.GetColor("#a278aa"))
+Rare.SetFillColor(ROOT.TColor.GetColor("#3e125f"))
+WH.SetLineWidth(4)
+ZH.SetLineWidth(4)
+
+Data.SetMarkerStyle(20)
+Data.SetLineColor(1)
+Data.SetMarkerSize(1)
+Fake.SetLineColor(1)
+if "ch9" in channels[0]:
+  WZ.SetLineColor(1)
+ZZ.SetLineColor(1)
+Rare.SetLineColor(1)
+Total.SetLineColor(1)
+ZH.SetLineColor(2)
+WH.SetLineColor(9)
+
+errorBand=Total.Clone()
+
+stack=ROOT.THStack("stack","stack")
+if "ch9" in channels[0]:
+  stack.Add(WZ)
+stack.Add(ZZ)
+stack.Add(Rare)
+stack.Add(Fake)
+
+errorBand.SetMarkerSize(0)
+errorBand.SetFillColor(new_idx)
+errorBand.SetFillStyle(3001)
+errorBand.SetLineWidth(1)
+
+pad1 = ROOT.TPad("pad1","pad1",0,0.35,1,1)
+pad1.Draw()
+pad1.cd()
+pad1.SetFillColor(0)
+pad1.SetBorderMode(0)
+pad1.SetBorderSize(10)
+pad1.SetTickx(1)
+pad1.SetTicky(1)
+pad1.SetLeftMargin(0.18)
+pad1.SetRightMargin(0.05)
+pad1.SetTopMargin(0.122)
+pad1.SetBottomMargin(0.026)
+pad1.SetFrameFillStyle(0)
+pad1.SetFrameLineStyle(0)
+pad1.SetFrameLineWidth(3)
+pad1.SetFrameBorderMode(0)
+pad1.SetFrameBorderSize(10)
+
+Data.GetXaxis().SetLabelSize(0)
+
+Data.SetMaximum(max(Data.GetMaximum()*1.35,stack.GetMaximum()*1.35))
+for k in range(1,Data.GetSize()-1):
+     s=WH.GetBinContent(k)+ZH.GetBinContent(k)
+     b=ZZ.GetBinContent(k)+Fake.GetBinContent(k)
+     if "ch9" in channels[0]:
+       b=ZZ.GetBinContent(k)+Fake.GetBinContent(k)+WZ.GetBinContent(k)
+     if (b<0):
+         b=0.000001
+     if (0.2*s/(0.00001+0.05*s+b)**0.5 > 0.15):
+         Data.SetBinContent(k,-10)
+         Data.SetBinError(k,0)
+Data.SetMinimum(0)
+Data.SetMaximum(1.3*Data.GetMaximum())
+
+Poisson=convert(Data)
+Poisson.GetXaxis().SetTitle("")
+Poisson.GetXaxis().SetTitleSize(0)
+Poisson.GetXaxis().SetNdivisions(505)
+Poisson.GetYaxis().SetLabelFont(42)
+Poisson.GetYaxis().SetLabelOffset(0.01)
+Poisson.GetYaxis().SetLabelSize(0.06)
+Poisson.GetYaxis().SetTitleSize(0.075)
+Poisson.GetYaxis().SetTitleOffset(1.04)
+Poisson.SetTitle("")
+Poisson.GetYaxis().SetTitle("Events/bin")
+Poisson.SetMaximum(max(Poisson.GetMaximum()*1.45,stack.GetMaximum()*1.45))
+Poisson.SetMinimum(0)
+Poisson.SetMarkerStyle(20)
+Poisson.SetLineColor(1)
+Poisson.SetMarkerSize(1)
+Poisson.GetXaxis().SetLabelSize(0)
+
+WH.SetMaximum(max(Poisson.GetMaximum()*1.45,stack.GetMaximum()*1.45))
+
+# Check all bins to see if bin yield is zero (this is happening
+# to WH postfit for some reason...) and limit range if so
+emptyBins = []
+for b in range(1, Total.GetXaxis().GetNbins()+ 1 ) :
+    if Total.GetBinContent(b) == 0 and Data.GetBinContent(b) == 0 :
+        print "both equal zero. skip bin",b
+        emptyBins.append( b )
+print "To skip:",emptyBins
+
+if len( emptyBins ) > 0 :
+    WH.GetXaxis().SetRange(1,emptyBins[0])
+    print "Limiting range from bins: %i - %i" % (1, emptyBins[0] )
+
+#Poisson.Draw("AP")
+#Data.Draw("e0")
+WH.Draw("hist")
+stack.Draw("histsame")
+errorBand.Draw("e2same")
+WH.Draw("histsame")
+ZH.Draw("histsame")
+#Data.Draw("e0same")
+Poisson.Draw("P")
+
+legende=make_legend()
+legende.AddEntry(Data,"Observed","elp")
+if "ch9" in channels[0]:
+  legende.AddEntry(WZ,"WZ#rightarrow 3l#nu","f")
+legende.AddEntry(ZZ,"ZZ#rightarrow 4l","f")
+legende.AddEntry(Rare,"Rare","f")
+legende.AddEntry(Fake,"Reducible","f")
+legende.AddEntry(WH,"WH, H#rightarrow#tau#tau (x5)","l")
+legende.AddEntry(ZH,"ZH, H#rightarrow#tau#tau (x5)","l")
+legende.AddEntry(errorBand,"Uncertainty","f")
+legende.Draw()
+
+l1=add_lumi()
+l1.Draw("same")
+l2=add_CMS()
+l2.Draw("same")
+l3=add_Preliminary()
+l3.Draw("same")
+
+pad1.RedrawAxis()
+
+categ  = ROOT.TPaveText(0.21, 0.45+0.013, 0.38, 0.70+0.155, "NDC")
+categ.SetBorderSize(   0 )
+categ.SetFillStyle(    0 )
+categ.SetTextAlign(   12 )
+categ.SetTextSize ( 0.05 )
+categ.SetTextColor(    1 )
+categ.SetTextFont (   42 )
+if fs=="emt":       
+  categ.AddText("e#mu#tau_{h}")
+if fs=="mmt":     
+  categ.AddText("#mu#mu#tau_{h}")
+if fs=="mtt":     
+  categ.AddText("#mu#tau_{h}#tau_{h}")
+if fs=="ett":     
+  categ.AddText("e#tau_{h}#tau_{h}")
+if fs=="eeem":     
+  categ.AddText("ee+e#mu")
+if fs=="eeet":
+  categ.AddText("ee+e#tau_{h}")
+if fs=="eemt":
+  categ.AddText("ee+#mu#tau_{h}")
+if fs=="eett":
+  categ.AddText("ee+#tau_{h}#tau_{h}")
+if fs=="emmm":
+  categ.AddText("#mu#mu+e#mu")
+if fs=="emmt":
+  categ.AddText("#mu#mu+e#tau_{h}")
+if fs=="mmmt":
+  categ.AddText("#mu#mu+#mu#tau_{h}")
+if fs=="mmtt":
+  categ.AddText("#mu#mu+#tau_{h}#tau_{h}")
+if fs=="whlep":
+  categ.AddText("WH semi-leptonic")
+if fs=="whhad":
+  categ.AddText("WH hadronic")
+if fs=="zh":
+  categ.AddText("ZH combined")
+
+categ.Draw()
+
+c.cd()
+pad2 = ROOT.TPad("pad2","pad2",0,0,1,0.35);
+pad2.SetTopMargin(0.05);
+pad2.SetBottomMargin(0.35);
+pad2.SetLeftMargin(0.18);
+pad2.SetRightMargin(0.05);
+pad2.SetTickx(1)
+pad2.SetTicky(1)
+pad2.SetFrameLineWidth(3)
+pad2.SetGridx()
+pad2.SetGridy()
+pad2.Draw()
+pad2.cd()
+h1=Data.Clone()
+hp=Poisson.Clone()
+hp.SetMaximum(2.0)#FIXME(1.5)
+hp.SetMinimum(0.0)#FIXME(0.5)
+hp.SetMarkerStyle(20)
+h1.SetMaximum(2.0)#FIXME(1.5)
+h1.SetMinimum(0.0)#FIXME(0.5)
+h1.SetMarkerStyle(20)
+h3=errorBand.Clone()
+hwoE=errorBand.Clone()
+p_x=hp.GetX()
+p_y=hp.GetY()
+print p_x[0],p_x[1],hwoE.GetBinContent(1)
+for iii in range (0,hwoE.GetSize()-2):
+  hwoE.SetBinError(iii+1,0)
+  print iii,p_x[iii],p_y[iii],p_y[iii]/max(hwoE.GetBinContent(iii+1),1e-5),h3.GetBinContent(iii+1)
+  hp.SetPoint(iii,p_x[iii],p_y[iii]/max(hwoE.GetBinContent(iii+1),1e-5))
+  hp.SetPointEYhigh(iii,hp.GetErrorYhigh(iii)/max(hwoE.GetBinContent(iii+1),1e-5))
+  hp.SetPointEYlow(iii,hp.GetErrorYlow(iii)/max(hwoE.GetBinContent(iii+1),1e-5))
+h1.SetStats(0)
+h1.Divide(hwoE)
+h3.Divide(hwoE)
+h1.GetXaxis().SetTitle("m_{vis} (GeV)")
+if "ch9" not in channels[0]:
+   h1.GetXaxis().SetTitle("m_{#tau#tau} (GeV)")
+h1.GetXaxis().SetLabelSize(0.08)
+h1.GetYaxis().SetLabelSize(0.08)
+h1.GetYaxis().SetTitle("Obs./Exp.")
+h1.GetXaxis().SetNdivisions(505)
+h1.GetYaxis().SetNdivisions(5)
+
+h1.GetXaxis().SetTitleSize(0.15)
+h1.GetYaxis().SetTitleSize(0.15)
+h1.GetYaxis().SetTitleOffset(0.56)
+h1.GetXaxis().SetTitleOffset(1.04)
+h1.GetXaxis().SetLabelSize(0.11)
+h1.GetYaxis().SetLabelSize(0.11)
+h1.GetXaxis().SetTitleFont(42)
+h1.GetYaxis().SetTitleFont(42)
+
+h3.GetXaxis().SetTitle("m_{vis} (GeV)")
+if "ch9" not in channels[0]:
+   h3.GetXaxis().SetTitle("m_{#tau#tau} (GeV)")
+h3.GetXaxis().SetLabelSize(0.08)
+h3.GetYaxis().SetLabelSize(0.08)
+h3.GetYaxis().SetTitle("Obs./Exp.")
+h3.GetXaxis().SetNdivisions(505)
+h3.GetYaxis().SetNdivisions(5)
+h3.SetTitle("")
+h3.GetXaxis().SetTitleSize(0.15)
+h3.GetYaxis().SetTitleSize(0.15)
+h3.GetYaxis().SetTitleOffset(0.56)
+h3.GetXaxis().SetTitleOffset(1.04)
+h3.GetXaxis().SetLabelSize(0.11)
+h3.GetYaxis().SetLabelSize(0.11)
+h3.GetXaxis().SetTitleFont(42)
+h3.GetYaxis().SetTitleFont(42)
+
+h3.SetMaximum(3.0)#FIXME(1.5)
+h3.SetMinimum(0.0)#FIXME(0.5)
+
+if len( emptyBins ) > 0 :
+    h3.GetXaxis().SetRange(1,emptyBins[0])
+    print "Limiting range from bins: %i - %i" % (1, emptyBins[0] )
+
+h3.Draw("e2")
+hp.Draw("P")
+
+c.cd()
+pad1.Draw()
+
+ROOT.gPad.RedrawAxis()
+
+c.Modified()
+c.SaveAs("final/"+fs+"_postfit.pdf")
+
+
---
+++ VH2016/poisson.py
@@ -0,0 +1,83 @@
+'''
+Convert a TH1 histogram into a TGraphAsymmErrors, with the proper poisson
+statistics.
+These use the Neyman construction, instead of simply sqrt(N).
+Code taken from:
+    https://twiki.cern.ch/twiki/bin/view/CMS/PoissonErrorBars
+   double gamma_quantile_c(double z, double alpha, double theta) {
+      return theta * ROOT::Math::Cephes::igami( alpha, z);
+   }
+   double gamma_quantile(double z, double alpha, double theta) {
+      // if possible, should use MathMore function ROOT::Math::gamma_quantile for z close to zero
+      // otherwise will always return zero for z  value smaller than eps
+      return theta * ROOT::Math::Cephes::igami( alpha, 1.- z);
+   }
+'''
+
+import ROOT
+from array import array
+from math import sqrt
+
+def poisson_errors(N, coverage=0.6827):
+    alpha = 1.0-coverage
+    L, U = 0, 0
+    if N>0:
+      L = ROOT.Math.gamma_quantile(alpha/2, N, 1.)
+    U = ROOT.Math.gamma_quantile_c(alpha/2,N+1,1)
+    return L, U
+
+
+def convert(histogram):
+    ''' Convert a histogram into a TGraphAsymmErrors with Poisson errors '''
+    output = ROOT.TGraphAsymmErrors(histogram)
+
+    for i in range(0,histogram.GetSize()-2):
+        yield_in_bin = output.GetY()[i]
+    	if yield_in_bin<0:
+    	    yield_in_bin=0
+        N=int(yield_in_bin)
+
+        L, U = poisson_errors(N)
+
+        output.SetPointEYlow(i, N - L)
+        output.SetPointEYhigh(i, U - N)
+
+    return output
+
+# Function to create TH1Fs from TGraphAsymmErrors
+def getTH1FfromTGraphAsymmErrors( asym, name='asym' ) :
+
+    # Holding vals for TH1F binning and y-vals
+    xSpacing = array( 'd', [] )
+    yVals = array( 'd', [] )
+    yErrors = array( 'd', [] )
+
+    nVals = asym.GetN()
+    x, y = ROOT.Double(0.), ROOT.Double(0.)
+    xEPlus, xEMin = 0., 0.
+    yEPlus, yEMin = 0., 0.
+
+    for n in range( nVals ) :
+        asym.GetPoint( n, x, y )
+        xEPlus = asym.GetErrorXhigh( n )
+        xEMin = asym.GetErrorXlow( n )
+        yEPlus = asym.GetErrorYhigh( n )
+        yEMin = asym.GetErrorYlow( n )
+        xSpacing.append( x-xEMin )
+        yVals.append( y )
+        # To simplify, take asymm errors and got to Gaussian
+        # for TH1
+        yErrors.append( sqrt(yEPlus**2 + yEMin**2) )
+
+    # Don't forget to add the high end of last bin
+    xSpacing.append( x+xEPlus )
+
+    #print xSpacing
+    #print yVals
+
+    outH = ROOT.TH1F( name, name, len(xSpacing)-1, xSpacing )
+    for bin in range( 1, outH.GetNbinsX()+1 ) :
+        outH.SetBinContent( bin, yVals[bin-1] )
+        outH.SetBinError( bin, yErrors[bin-1] )
+    return outH
+
